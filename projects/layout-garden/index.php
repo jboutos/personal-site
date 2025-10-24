b482914626fd264fd2b630548519be8b94ea7876
jboutos <215729236+jboutos@users.noreply.github.com>
2025-10-24 14:59:02 +0300

    Added some fonts

--- projects/layout-garden/index.php
+++ projects/layout-garden/index.php
@@ -6,6 +6,7 @@
 		<meta name='viewport' content='width=device-width, initial-scale=1'>
 		<meta name='description' content='A compilation of layouts'>
 		<meta property='og:image' content='[[meta image source]]'>
+		<link href="https://fonts.googleapis.com/css?family=Merriweather|Mulish:300" rel="stylesheet">
 		<link rel='stylesheet' href='css/style.css'>
 	</head>
 	<body>   
