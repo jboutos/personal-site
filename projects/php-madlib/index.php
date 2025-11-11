a91a089e650b25d47e9523170b06fb05f622035c
jboutos <215729236+jboutos@users.noreply.github.com>
2025-11-11 17:22:54 +0200

    various changes

---
+++ projects/php-madlib/index.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html>
+	<head>
+	</head>
+	<body>
+		<?php
+			$place = "convenience store";
+			$attribute = "delicious";
+			$noun = "meal";
+			$animal = "bear";
+			$verb = "walked";
+			$verb2 = "shouting";
+
+			echo "Today, I went to the " . $place . " to buy a " . $attribute . " " . $noun .".
+			When I got there, a ". $animal . " suddenly " . $verb . " past me!
+			Everyone started " . $verb2 . ", and I couldn’t stop " . $verb2 . " too."
+		?>
+
+		<p>Today, I went to the <?php echo $place;?> to buy a <?php echo $attribute;?> <?php echo $noun;?>.
+			When I got there, a <?php echo $animal;?> suddenly <?php echo $verb;?> past me!
+			Everyone started <?php echo $verb2;?>, and I couldn’t stop <?php echo $verb2;?> too.</p>
+
+		<p>
+			Today, I went to the <?=$place?> to buy a <?=$attribute?> <?=$noun?>.
+			When I got there, a <?=$animal?> suddenly <?=$verb?> past me!
+			Everyone started <?=$verb2?>, and I couldn’t stop <?=$verb2?> too.
+		</p>
+		
+	</body>
+</html>
