6f1380fa856f0e56bbf80be03dacde50cccfd167
jboutos <215729236+jboutos@users.noreply.github.com>
2025-11-22 19:27:07 +0200

    name-change

--- projects/php-exercises/exercise-17/blood-alcohol-calculator.php
+++ projects/php-exercises/exercise-17/blood-alcohol-calculator.php
@@ -1,86 +1,88 @@
-<a id='blood'></a>
+<blood-alcohol>
+	<a id='blood'></a>
 
-<form method='POST'>
+	<form action='index.php#blood' method='POST'>
 
-	<h3>Blood alcohol calculator</h3>
-	<p>Calculates the blood alcohol content</p>
+		<h3>Blood alcohol calculator</h3>
+		<p>Calculates the blood alcohol content</p>
 
-	<div class='field'>
-		<label for='weight'>Weight</label>
-		<input id='weight' type='number' name='weight' min='1' step='0.1' value="<?php echo $_POST['weight'] ?? ''; ?>">
-	</div>
+		<div class='field'>
+			<label for='weight'>Weight</label>
+			<input id='weight' type='number' name='weight' min='1' step='0.1' value="<?php echo $_POST['weight'] ?? ''; ?>">
+		</div>
 
-	<div class='field'>
-		<label for='gender'>Gender</label>
-		<input id='gender' type='text' name='gender' value="<?php echo $_POST['gender'] ?? ''; ?>">
-	</div>
+		<div class='field'>
+			<label for='gender'>Gender</label>
+			<input id='gender' type='text' name='gender' value="<?php echo $_POST['gender'] ?? ''; ?>">
+		</div>
 
-	<div class='field'>
-		<label for='drinks'>Total oz of drinks</label>
-		<input id='drinks' type='number' name='drinks' step='0.1' min='0' value="<?php echo $_POST['drinks'] ?? ''; ?>">
-	</div>
+		<div class='field'>
+			<label for='drinks'>Total oz of drinks</label>
+			<input id='drinks' type='number' name='drinks' step='0.1' min='0' value="<?php echo $_POST['drinks'] ?? ''; ?>">
+		</div>
 
-	<div class='field'>
-		<label for='volume'>Alcohol amount by volume</label>
-		<input id='volume' type='number' name='volume' step='0.1' min='0' value="<?php echo $_POST['volume'] ?? ''; ?>">
-	</div>
+		<div class='field'>
+			<label for='volume'>Alcohol amount by volume</label>
+			<input id='volume' type='number' name='volume' step='0.1' min='0' value="<?php echo $_POST['volume'] ?? ''; ?>">
+		</div>
 
-	<div class='field'>
-		<label for='time'>Hours passed since last drink</label>
-		<input id='time' type='number' name='time' min='0' value="<?php echo $_POST['time'] ?? ''; ?>">
-	</div>
+		<div class='field'>
+			<label for='time'>Hours passed since last drink</label>
+			<input id='time' type='number' name='time' min='0' value="<?php echo $_POST['time'] ?? ''; ?>">
+		</div>
 
-	<button type='submit' name='blood-submit'>Submit</button>
-</form>
+		<button type='submit' name='blood-submit'>Submit</button>
+	</form>
 
-<?php
+	<?php
 
-	if (isset($_POST['blood-submit'])) {
+		if (isset($_POST['blood-submit'])) {
 
-		if (isset($_POST['weight'])) {
-			$weight = $_POST['weight'];
+			if (isset($_POST['weight'])) {
+				$weight = $_POST['weight'];
+			}
+
+			if (isset($_POST['gender'])) {
+				$gender = strtolower($_POST['gender']);
+			}
+
+			if (isset($_POST['drinks'])) {
+				$drinks = $_POST['drinks'];
+			}
+
+			if (isset($_POST['volume'])) {
+				$volume = $_POST['volume'];
+			}
+
+			if (isset($_POST['time'])) {
+				$time = $_POST['time'];
+			}
+				
+			if (strlen($weight) == 0 || strlen($drinks) == 0 || strlen($volume) == 0 || strlen($time) == 0 ) {
+	    		echo "<p>All fields must have values!</p>";
+	    		return;
+			} else if ($gender !== "male" && $gender !== "female") {
+				echo "<p>Please enter a valid gender value</p>";
+	    		return;
+			}
+
+			if ($gender == "male") {
+				$r = 0.73;
+			} else {
+				$r = 0.66;
+			}
+
+			$a = $drinks * ($volume / 100);
+			$bac = (($a * 5.14) / ($weight * $r)) - (0.015 * $time);
+			$bac = round($bac, 2);
+
+			if ($bac < 0.08) {
+				echo "<p>Your BAC is " . $bac . "<br>It is legal for you to drive.</p>";
+			} else {
+				echo "<p>Your BAC is " . $bac . "<br>It is not legal for you to drive.</p>";
+			}
+
 		}
 
-		if (isset($_POST['gender'])) {
-			$gender = strtolower($_POST['gender']);
-		}
-
-		if (isset($_POST['drinks'])) {
-			$drinks = $_POST['drinks'];
-		}
-
-		if (isset($_POST['volume'])) {
-			$volume = $_POST['volume'];
-		}
-
-		if (isset($_POST['time'])) {
-			$time = $_POST['time'];
-		}
-			
-		if (strlen($weight) == 0 || strlen($drinks) == 0 || strlen($volume) == 0 || strlen($time) == 0 ) {
-    		echo "<p>All fields must have values!</p>";
-    		return;
-		} else if ($gender !== "male" && $gender !== "female") {
-			echo "<p>Please enter a valid gender value</p>";
-    		return;
-		}
-
-		if ($gender == "male") {
-			$r = 0.73;
-		} else {
-			$r = 0.66;
-		}
-
-		$a = $drinks * ($volume / 100);
-		$bac = (($a * 5.14) / ($weight * $r)) - (0.015 * $time);
-		$bac = round($bac, 2);
-
-		if ($bac < 0.08) {
-			echo "<p>Your BAC is " . $bac . "<br>It is legal for you to drive.</p>";
-		} else {
-			echo "<p>Your BAC is " . $bac . "<br>It is not legal for you to drive.</p>";
-		}
-
-	}
-
-?>
+	?>
+</blood-alcohol>
--- projects/php-exercises/exercise-18/temperature-converter.php
+++ projects/php-exercises/exercise-18/temperature-converter.php
@@ -1,1 +1,128 @@
-temperature-converter.php
+<style>
+	inner-column {
+		display: flex;
+		flex-direction: column;
+		max-width: 1600px;
+		margin-left: auto;
+		margin-right: auto;
+	}
+
+	.field {
+		display: flex;
+		flex-direction: column;
+	}
+
+	header {
+		@media(min-width: 900px) {
+			display: flex;
+			flex-direction: column;
+			align-items: center;
+		}
+	}
+
+	footer {
+		@media(min-width: 900px) {
+			display: flex;
+			flex-direction: column;
+			align-items: center;
+		}
+	}
+
+	header a {
+		margin-bottom: 1em;
+		color: brown;
+	}
+
+	footer a {
+		margin-top: 2em;
+		color: brown;
+	}
+
+	form {
+		max-width: 300px;
+		background: linear-gradient(150deg, lightgray, #F2F0EF);
+		padding: 1em;
+		border: 1px solid black;
+		border-radius: 10px;
+		box-shadow: rgba(99, 99, 99, 0.2) 0px 2px 8px 0px;
+	}
+
+	button[type='submit'] {
+		margin-top: 20px;
+		border-radius: 15px;
+		border: 1px solid black;
+		box-shadow: rgba(99, 99, 99, 0.2) 0px 2px 8px 0px;
+		padding: 3px 7px;
+	}
+
+	input {
+		border-radius: 10px;
+		border: 1px solid gray;
+		padding: 3px 7px;
+	}
+
+	label {
+		opacity: 0.5;
+		margin: 0;
+		font-size: 13px;
+		padding-bottom: 5px;
+		font-family: 'Monda', sans-serif;
+
+	}
+
+	h3 {
+		color: brown;
+	}
+
+	.field + .field {
+		margin-top: 1.1em;
+	}
+
+	partials {
+		display: grid;
+		gap: 2em;
+
+		@media(min-width: 600px) {
+			display: grid;
+			grid-template-columns: repeat(2, 1fr);
+			column-gap: 1em;
+			row-gap: 2em;
+		}
+
+		@media(min-width: 750px) {
+			display: grid;
+			grid-template-columns: repeat(3, 1fr);
+		}
+
+		@media(min-width: 900px) {
+			display: grid;
+			grid-template-columns: repeat(4, 1fr);
+		}
+	}
+
+	.mini {
+		font-size: 13px;
+	}
+
+	p, a {
+		font-family: 'Monda', sans-serif;
+		font-size: 16px;
+	}
+
+	h1, h2, h3, h4 {
+		font-family: 'Julius Sans One', sans-serif;
+	}
+</style>
+
+<form method='POST'>
+
+	<h3>Temperature Converter</h3>
+	<p>Converts temperatures from Fahrenheit to Celsius and vice versa</p>
+
+	<div class='field'>
+		<label></label>
+		<input>
+	</div>
+
+	<button type='submit' name='temperature-submit'>Submit</button>
+</form>
