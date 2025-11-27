811a75727a6ff9b16e5b6c94384f168123c1e263
jboutos <215729236+jboutos@users.noreply.github.com>
2025-11-27 15:53:19 +0200

    more styles

--- projects/shoelace-haven/details.php
+++ projects/shoelace-haven/details.php
@@ -13,26 +13,31 @@
 	}
 ?>
 
-<?php if (isset($detail)) { 
-	
+	<?php if (isset($detail)) { 
+		
 
-	$description2 = "Our " . strtolower($detail["name"]) . " are crafted for durability and style, available in " . $detail["size"]
-		        . " sizes, with a " . $detail["color"] . " of colors to match any shoe or outfit. " . "Prices start as low as " . $detail["cost"] . " euros, "
-		        . ($detail["availability"] ? "currently in stock and ready to wear!" : "but currently out of stock, grab them when available.");
-?>
+		$description2 = "Our " . strtolower($detail["name"]) . " are crafted for durability and style, available in " . $detail["size"]
+			        . " sizes, with a " . $detail["color"] . " of colors to match any shoe or outfit. " . "Prices start as low as " . $detail["cost"] . " euros, "
+			        . ($detail["availability"] ? "currently in stock and ready to wear!" : "but currently out of stock, grab them when available.");
+	?>
 
-	<description>
+		<description>
 
-		<picture>
-			<img src='<?=$detail["image"]?>'>
-		</picture>
+			<picture>
+				<img src='<?=$detail["image"]?>'>
+			</picture>
 
-		<div class='description'>
+			<div class='description'>
 
-			<h1 class='attention-voice'><?=$detail['name']?></h1>
-			<p class='calm-voice'><?=$description2?></p>
+				<h1 class='attention-voice'><?=$detail['name']?></h1>
+				<p class='calm-voice'><?=$description2?></p>
+				<?php if ($detail["availability"]) { ?>
+					<a href='#'>Add to cart</a>
+				<?php } else { ?>
+					<span>Restock soon</span>
+				<?php } ?>
 
-		</div>
+			</div>
 
 <?php } else {?>
 
