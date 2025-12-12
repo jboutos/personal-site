406c13152ae0cff4d59d78468ade17426018e6b4
jboutos <215729236+jboutos@users.noreply.github.com>
2025-12-12 17:00:02 +0200

    fix: message board sizing

--- 4-page-website/contact.php
+++ 4-page-website/contact.php
@@ -37,9 +37,13 @@
 	}
 
 	.field {
+		@media (max-width: 470px) {
+			width: 90%;
+			margin: 0 auto;
+		}
+
 		display: flex;
 		flex-direction: column;
-		max-width: 400px;
 	}
 
 	textarea#message {
@@ -57,10 +61,13 @@
 
 	input, textarea {
 		border: 1px solid darkmagenta;
-		min-width: 90%;
 		padding: 10px;
 		font-size: 1rem;
 		box-shadow: rgba(14, 63, 126, 0.06) 0px 0px 0px 1px, rgba(42, 51, 70, 0.03) 0px 1px 1px -0.5px, rgba(42, 51, 70, 0.04) 0px 2px 2px -1px, rgba(42, 51, 70, 0.04) 0px 3px 3px -1.5px, rgba(42, 51, 70, 0.03) 0px 5px 5px -2.5px, rgba(42, 51, 70, 0.03) 0px 10px 10px -5px, rgba(42, 51, 70, 0.03) 0px 24px 24px -8px;
+
+		@media (min-width: 470px) {
+			width: 400px;
+		}
 	}
 
 	button {
