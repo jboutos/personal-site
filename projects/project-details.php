674f8e57b5a580d656fd4d821c4206aedc3129b9
jboutos <215729236+jboutos@users.noreply.github.com>
2025-12-18 14:44:40 +0200

    fix: minor fix

--- projects/project-details.php
+++ projects/project-details.php
@@ -99,7 +99,7 @@
 <?php include("projects-data.php") ?>
 
 <?php
-	//needed the data as a partial, if it wss kept in projects2 the array wouldnt load since index.php includes only this page and not projects2!
+	//needed the data as a partial, if it was kept in projects2 the array wouldnt load since index.php includes only this page and not projects2!
 
 	$project_id = '';
 
@@ -107,16 +107,17 @@
 		$project_id = $_GET['project'];
 	
 
-	foreach ($projects as $project) {
-		if ($project_id == $project["id"]) {
-			$detail = $project;
-			break;
+		foreach ($projects as $project) {
+			if ($project_id == $project["id"]) {
+				$detail = $project;
+				break;
+			}
 		}
 	}
 ?>
 
 <?php if (isset($detail)) { ?>
-	<?php component("project-detail", $detail); } ?>
+    <?php component("project-detail", $detail); ?>
 <?php } else { ?>
-	<?php component("project-not-found"); ?>
+    <?php component("project-not-found"); ?>
 <?php } ?>
