1a4c91bca7908145d6df531f29db892f244be3db
jboutos <215729236+jboutos@users.noreply.github.com>
2026-03-13 20:18:38 +0200

    feat: separate project card components

---
+++ components/project-card-solo.php
@@ -0,0 +1,12 @@
+<?php 
+   $href = !empty($directLink) ? $directLink : "?page=details&project={$key}";
+?>
+<project-card id='<?= $id ?>'>
+   <picture class='image'>
+      <a href='<?= $href ?>'>
+         <img src='<?= $image ?>' alt='<?= $title ?> image'>
+      </a>
+   </picture>
+   <h2 class='subtle-voice'><?= $title ?></h2>
+   <a class='underline' href='<?= $href ?>'>See more</a>
+</project-card>
--- components/project-card.php
+++ components/project-card.php
@@ -1,18 +1,7 @@
 <ul>
    <?php foreach ($cards as $card) : ?>
       <li>
-         <?php 
-            $href = !empty($card['directLink']) ? $card['directLink'] : "?page=details&project={$card['key']}";
-         ?>
-         <project-card id='<?= $card['id'] ?>'>
-            <picture class='image'>
-               <a href='<?= $href ?>'>
-                  <img src='<?= $card['image'] ?>' alt='<?= $card['title'] ?> image'>
-               </a>
-            </picture>
-            <h2 class='subtle-voice'><?= $card['title'] ?></h2>
-            <a class='underline' href='<?= $href ?>'>See more</a>
-         </project-card>
+         <?= component('project-card-solo', $card) ?>
       </li>
    <?php endforeach; ?>
 </ul>
