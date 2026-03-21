a4b2eb4789786594a7d6bb5fce0f4d9a04b68183
jboutos <215729236+jboutos@users.noreply.github.com>
2026-03-22 00:36:43 +0200

    fix: some colors in garden and site

--- projects/layout-garden/module-7/footer-1.php
+++ projects/layout-garden/module-7/footer-1.php
@@ -2,7 +2,7 @@
 	<inner-column>
 		<div class='container-2'>
 			<div class='contact'>
-				<h3 class='subtle-voice'>Contact</h3>
+				<h3 class='wise-voice'>Contact</h3>
 				<p class='calm-voice'>address line 1<br>address line 2</p>
 				<p class='calm-voice'>e-mail</p>
 				<p class='calm-voice'>phone</p>
@@ -20,21 +20,21 @@
 				</div>
 			</div>
 			<div class='choices'>
-				<h3 class='subtle-voice'>Choices</h3>
+				<h3 class='wise-voice'>Choices</h3>
 				<a href='#' class='info'>Company</a>
 				<a href='#' class='info'>Services</a>
 				<a href='#' class='info'>Customers</a>
 				<a href='#' class='info'>Contact</a>
 			</div>
 			<div class='services'>
-				<h3 class='subtle-voice'>Services</h3>
+				<h3 class='wise-voice'>Services</h3>
 				<a href='#' class='info'>Something</a>
 				<a href='#' class='info'>Something</a>
 				<a href='#' class='info'>Something</a>
 				<a href='#' class='info'>Something</a>
 			</div>
 			<div class='our-work'>
-				<h3 class='subtle-voice'>Our work</h3>
+				<h3 class='wise-voice'>Our work</h3>
 				<div class='container'>
 					<div>
 						<a href='#'>
